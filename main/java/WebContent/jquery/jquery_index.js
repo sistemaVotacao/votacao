c9547665c3bc6125769dfb858548e1d0bc800f86
sistemaVotacao <50084352+sistemaVotacao@users.noreply.github.com>
2019-04-30 08:12:38 -0300

    Add files via upload

---
+++ main/java/WebContent/jquery/jquery_index.js
@@ -0,0 +1,24 @@
+$(function(){
+	$('#btVotar').click(function(){
+		$("#form").attr("action","sistema?log=ValidarUsuario")
+		$('#containerForm').toggleClass('tamanhoContainerLogin');
+		$('#containerBotoes').hide();
+		$('#containerForm').slideDown(500);
+	});
+	$('#btAreaMesario').click(function(){
+		$("#form").attr("action","sistema?log=ValidarMesario");
+		$('#containerForm').toggleClass('tamanhoContainerLogin');
+		$('#containerBotoes').hide();
+		$('#containerForm').slideDown(500);
+	});
+	$('#btAreaSupervisor').click(function(){
+		$("#form").attr("action","sistema?log=ValidarChefe");
+		$('#containerForm').toggleClass('tamanhoContainerLogin');
+		$('#containerBotoes').hide();
+		$('#containerForm').slideDown(500);
+	});
+	$('#btVoltar').click(function(){
+		$('#containerForm').hide();
+		$('#containerBotoes').slideDown(500);
+	});
+});
